a9328b5ee808489164ff9377128c075385719d70
Sergey Ukustov <sergey@ukstv.me>
2024-03-13 08:23:08 +0300

    api

--- packages/skyward/src/local/client.ts
+++ packages/skyward/src/local/client.ts
@@ -2,6 +2,6 @@ import { initClient } from "@ts-rest/core";
 import { HttpSchema } from "./http-schema.js";
 
 export const CLIENT = initClient(HttpSchema, {
-  baseUrl: "https://skyward-api-rbmp3gpe7q-ez.a.run.app/",
+  baseUrl: "https://api.swrd.ukstv.me/",
   baseHeaders: {},
 });
